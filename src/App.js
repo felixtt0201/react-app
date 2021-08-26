ea567788aa7a6ea93159d3a05b7e5f2cd46e70b7
Bentt0201 <bem0507@hotmail.com>
2021-08-26 22:41:54 +0800

    feat: headerComponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// Components
+import Header from './components/Header';
+
+// style
+import { GlobalStyle } from './GlobalStyle';
+
+// start test without JSX just for test
+// const Star = () =>
+//   React.createElement('div', null, 'This is my first ReactComponet');
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      Start here.
+      <GlobalStyle />
     </div>
   );
-}
+};
 
 export default App;
