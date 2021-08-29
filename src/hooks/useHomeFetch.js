c228d05d04caa9c40c5f2e42fe75b77a1bd7e29f
Bentt0201 <bem0507@hotmail.com>
2021-08-29 21:39:58 +0800

    feat: header and headerImage

---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,44 @@
+// Hooks
+import { useState, useEffect, useRef } from 'react';
+
+// API
+import API from '../API';
+
+// 初始化state
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchIerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchIerm, page);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  return { state, loading, error };
+};
