06da33826ef31932982159adce5ce2ff131f0744
Bentt0201 <bem0507@hotmail.com>
2021-08-29 22:11:02 +0800

    feat: add Thumb Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
 
 // Custome  Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -28,7 +29,16 @@ const Home = () => {
       ) : null}
       <Grid header="Pouplar Movies">
         {state.results.map((movie) => (
-          <div key={movie.id}>{movie.title}</div>
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          ></Thumb>
         ))}
       </Grid>
     </>
---
+++ src/components/Thumb/index.js
@@ -0,0 +1,12 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+import { Image } from './Thumb.style';
+
+// Thumb Component
+const Thumb = ({ image, movieId, clickable }) => (
+  <div>
+    <Image src={image} alt="movie-thumb" />
+  </div>
+);
+
+export default Thumb;
