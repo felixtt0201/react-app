4ee9e5918122c361c1fc097aea5d05388d9f44b3
Bentt0201 <bem0507@hotmail.com>
2021-08-31 16:04:44 +0800

    feat: add BreadCrumb Component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
 
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
@@ -18,9 +19,11 @@ const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
   console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Someting went wrong...</div>;
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   );
 };
